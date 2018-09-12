24eaaead92205711cb244442301b92ab3137bba9
Yajingleo <yajingleo@math.ucla.edu>
2018-09-11 22:35:21 -0400

    Create NextClosetTime.cpp

---
+++ NextClosetTime.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string nextClosestTime(string time) {
+        set<char> letters;
+        for (char c: time) {
+            if (c != ':') letters.insert(c);
+        }
+        set<string> Times;
+        for (char c1: letters) {
+            for (char c2: letters) {
+                for (char c3: letters) {
+                    for (char c4: letters) {
+                        if (c1 <= '2' && (c2 <= '4' || c1 <= '1') && c3 <= '5') {
+                            char s[] = {c1, c2, ':', c3, c4};
+                            Times.insert(string(s)); // bug!
+                        }
+                    }
+                }
+            }
+        }
+        
+        set<string>::iterator it = times.find(time);
+        ++it;
+        
+        return (it == times.end()) ? *times.begin() : *it;
+    }
+};
